cc9552939e9eb41e5a5449de7e1236cbd70338c7
Jose Santos Lopez <jsljr@ad.unc.edu>
2021-10-29 21:55:25 -0400

    shiny_app_01.R code updated

--- script/shiny_app_01.R
+++ script/shiny_app_01.R
@@ -3,5 +3,23 @@
 # Author: Jose S. Lopez
 
 # library setup
-library(tidyverse);
-library(shiny);
+library(tidyverse)
+library(shiny)
+library(plotly)
+
+hmdata_5 <- read_csv("derived_data/hmdata.csv", col_types = cols())
+
+ui <- fluidPage(
+  selectInput("choice", "Choose", choices = names(hmdata), selected = NULL),
+  plotlyOutput("graph")
+)
+
+server <- function(input, output, session){
+  
+  output$graph <- renderPlotly({
+    plot_ly(hmdata, x = ~get(input$choice), y = ~review_rating, type = 'scatter', mode = 'markers')
+  })
+}
+
+shinyApp(ui=ui, server=server,
+         options=list(port=8080, host="0.0.0.0"));
